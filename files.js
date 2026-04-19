31e1288b01d5f0f457df08f8483e61a7c1a473cd
space16071969-oss <space16071969@gmail.com>
2026-04-19 09:14:05 +0700

    Add FNAF series files to files.js

--- files.js
+++ files.js
@@ -10,18 +10,46 @@
 
 const files = [
     {
-        name: "Adobe Premiere Pro",
-        icon: "https://i.imgur.com/76QYdHV.png",
-        size: "2,06 GB",
-        description: "request keanu",
-        url: "https://limewire.com/d/FglqM#rgyCfQ6BdE"
+        name: "FNAF 1",
+        icon: "https://i.imgur.com/OGC2tU9.png",
+        size: "111.2 MB",
+        description: "series fnaf",
+        url: "https://an1.com/file_5768-dw_apk_o.html"
     },
     {
-        name: "belum ada wok",
-        icon: "-",
-        size: "- KB",
-        description: "tunggu admin tambah file",
-        url: "http://localhost:8000"
+        name: "FNAF 2",
+        icon: "https://an1.com/uploads/posts/2023-10/1696254774_five-nights-at-freddy-2.png",
+        size: "110 MB",
+        description: "series fnaf",
+        url: "https://an1.com/file_3969-dw_apk_o.html"
+    },
+    {
+        name: "FNAF 3",
+        icon: "https://an1.com/uploads/posts/2024-01/1704290543_fnaf3.png",
+        size: "62.4 MB",
+        description: "series fnaf",
+        url: "https://an1.com/file_260-dw_apk_o.html"
+    },
+    {
+        name: "FNAF 4",
+        icon: "https://an1.com/uploads/posts/2024-01/1704288680_fnaf4.png",
+        size: "53.7 MB",
+        description: "series fnaf",
+        url: "https://an1.com/file_553-dw_apk_o.html"
+    },
+    {
+        name: "FNAF 5",
+        icon: "https://an1.com/uploads/posts/2021-12/1640710654_fnaf5.png",
+        size: "283.6 MB",
+        description: "series fnaf",
+        url: "https://an1.com/file_4369-dw_apk_o.html"
+    },
+    {
+        name: "FNAF 6",
+        icon: "https://an1.com/uploads/posts/2024-01/1704188356_fnaf-6.png",
+        size: "153.1 MB",
+        description: "series fnaf",
+        url: "https://an1.com/file_5894-dw_apk_o.html"
     }
     
 ];
